d1e24a8426d96165a40947b65adbec10618bf03c
Vedant Ombase <vedantom@buffalo.edu>
2022-01-16 00:54:53 -0500

    cleaner adj node searching

--- day9/smokeBasin.py
+++ day9/smokeBasin.py
@@ -1,6 +1,7 @@
 import queue
 import heapq
 
+
 # Time: 1hr 4min
 # Part 1: 30min
 # Part 2: 34min
@@ -9,23 +10,23 @@ def basin(matrix):
     basinSizes = []
     lowPoints = []
     # first get all lowPoints in matrix
-    for x in range(0, len(matrix)):
-        for y in range(0, len(matrix[0])):
+    for x in range(len(matrix)):
+        for y in range(len(matrix[0])):
             if isLowPoint(matrix, x, y):
                 lowPoints.append([x, y])
 
     # calculate size of each Basin using BFS
     for lowPoint in lowPoints:
-        someQueue = queue.Queue(0)
+        Q = queue.Queue(0)
         visited = [lowPoint]
-        someQueue.put(lowPoint)
+        Q.put(lowPoint)
 
-        while someQueue.qsize() > 0:
-            coordinates = someQueue.get()
+        while Q.qsize() > 0:
+            coordinates = Q.get()
             for neighbor in getNeighbor(coordinates):
                 if neighbor not in visited:
                     visited.append(neighbor)
-                    someQueue.put(neighbor)
+                    Q.put(neighbor)
 
         heapq.heappush(basinSizes, len(visited))
 
@@ -41,24 +42,22 @@ def getNeighbor(coordinates):
     x = coordinates[0]
     y = coordinates[1]
 
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if i == 0 and j == 0:   # removing itself from check
-                continue
-            if (j == -1 or j == 1) and i != 0:  # removing corners from check\
-                continue
-            if (x+i >= 0) and (x+i < len(matrix)) and \
-                    (y+j >= 0) and (y+j < len(matrix[0])):
-                if matrix[x][y] < matrix[x + i][y + j] != 9:
-                    neighborList.append([x+ i, y + j])
+    delta_i = [-1, 0, 1, 0]
+    delta_j = [0, -1, 0, 1]
+
+    for d in range(4):
+        di = x + delta_i[d]
+        dj = y + delta_j[d]
+        if 0<=di<len(matrix) and 0<=dj<len(matrix[0]) and matrix[x][y] < matrix[di][dj] != 9:
+            neighborList.append([di, dj])
 
     return neighborList
 
 
-def sumLowPoints(matrix):
+def sumRiskPoints(matrix):
     sumRisk = 0
-    for x in range(0, len(matrix)):
-        for y in range(0, len(matrix[0])):
+    for x in range(len(matrix)):
+        for y in range(len(matrix[0])):
             if isLowPoint(matrix, x, y):
                 riskLevel = matrix[x][y] + 1
                 sumRisk += riskLevel
@@ -66,17 +65,14 @@ def sumLowPoints(matrix):
 
 
 def isLowPoint(matrix, x, y):
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if i == 0 and j == 0:   # dont check x,y
-                continue
-            if (j == -1 or j == 1) and i != 0:  # dont check corners
-                continue
+    delta_i = [-1, 0, 1, 0]
+    delta_j = [0, -1, 0, 1]
 
-            if (x+i >= 0) and (x+i < len(matrix)) and \
-                    (y+j >= 0) and (y+j < len(matrix[0])):
-                if matrix[x][y] >= matrix[x+i][y+j]:
-                    return False
+    for d in range(4):
+        di = x + delta_i[d]
+        dj = y + delta_j[d]
+        if 0 <=di<len(matrix) and 0<=dj<len(matrix[0]) and matrix[x][y] >= matrix[di][dj]:
+            return False
 
     return True
 
@@ -87,5 +83,5 @@ for line in f:
     matrix.append([int(x) for x in line.rstrip("\n")])
 f.close()
 
-print(sumLowPoints(matrix))
+print(sumRiskPoints(matrix))
 print(basin(matrix))
