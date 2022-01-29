38cdca4d697b156524c5da85e5843333b8ba5949
Vedant Ombase <vedantom@buffalo.edu>
2022-01-29 03:18:38 -0500

    replace {} with Counter() -> no missing key check needed

--- day14/polymerization.py
+++ day14/polymerization.py
@@ -7,27 +7,27 @@ for line in pairing.split("\n"):
     p1, p2 = line.split(" -> ")
     rules[p1] = p2
 
-counter = {}
+counter = Counter()
 # add initial pairs
 for i in range(len(s)-1):
-    counter[s[i]+s[i+1]] = counter.get(s[i] + s[i+1], 0) + 1
+    counter[s[i]+s[i+1]] += 1
 
 for t in range(1, steps+1):
-    temp = {}
+    temp = Counter()
     # NN -> (NC, CN) both with same count as NN
     for k in counter:
-        temp[k[0] + rules[k]] = temp.get(k[0] + rules[k], 0) + counter[k]
-        temp[rules[k] + k[1]] = temp.get(rules[k] + k[1], 0) + counter[k]
+        temp[k[0] + rules[k]] += counter[k]
+        temp[rules[k] + k[1]] += counter[k]
 
     counter = temp
     # print part1 and part2
     if t in [10, 40]:
-        letter_count = {}
+        letter_count = Counter()
         for k in counter:
             # Every character appears in the first half of its pair except for the last character
-            letter_count[k[0]] = letter_count.get(k[0], 0) + counter[k]
+            letter_count[k[0]] += counter[k]
         # add 1 to count of last character, which remains unchanged from the original string
-        letter_count[s[-1]] = letter_count.get(s[-1], 0) + 1
+        letter_count[s[-1]] += 1
 
         print(f'At step {t}: {max(letter_count.values()) - min(letter_count.values())}')
 
