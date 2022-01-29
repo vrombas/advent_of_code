c6bc10fad781352d66db8e4de418888e6ce602f6
Vedant Ombase <vedantom@buffalo.edu>
2022-01-29 03:16:31 -0500

    day 14 - part 2

--- day14/polymerization.py
+++ day14/polymerization.py
@@ -1,21 +1,35 @@
 from collections import deque, defaultdict, Counter
-steps = 10
+steps = 40
 s, pairing = open("input.txt", 'r').read().split("\n\n")
 
 rules = {}
-# print(s)
 for line in pairing.split("\n"):
     p1, p2 = line.split(" -> ")
     rules[p1] = p2
 
-for _ in range(steps):
-    s_new = ""
-    for i in range(len(s)):
-        s_new += s[i]
-        if i+1 < len(s): # takes care of adding s[-1]
-            s_new += rules.get(s[i] + s[i+1], "")
-    s = s_new
-    # print(s)
+counter = {}
+# add initial pairs
+for i in range(len(s)-1):
+    counter[s[i]+s[i+1]] = counter.get(s[i] + s[i+1], 0) + 1
 
-count = Counter(s)
-print(max(count.values()) - min(count.values()))
+for t in range(1, steps+1):
+    temp = {}
+    # NN -> (NC, CN) both with same count as NN
+    for k in counter:
+        temp[k[0] + rules[k]] = temp.get(k[0] + rules[k], 0) + counter[k]
+        temp[rules[k] + k[1]] = temp.get(rules[k] + k[1], 0) + counter[k]
+
+    counter = temp
+    # print part1 and part2
+    if t in [10, 40]:
+        letter_count = {}
+        for k in counter:
+            # Every character appears in the first half of its pair except for the last character
+            letter_count[k[0]] = letter_count.get(k[0], 0) + counter[k]
+        # add 1 to count of last character, which remains unchanged from the original string
+        letter_count[s[-1]] = letter_count.get(s[-1], 0) + 1
+
+        print(f'At step {t}: {max(letter_count.values()) - min(letter_count.values())}')
+
+# I really need to do something about my input and output for these
+# switch back and forth between test.txt and input.txt have cost me so much time
