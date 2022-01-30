1e295a447be84a1e185eb1f118f8dda0dc932898
Vedant Ombase <vedantom@buffalo.edu>
2022-01-29 22:53:04 -0500

    Solved w/ Uniform-Cost Search

--- day15/chiton.py
+++ day15/chiton.py
@@ -1,33 +1,23 @@
 from collections import deque
 from queue import PriorityQueue
-import heapq
+# Note: Could've used heapq as well (heappush & heappop)
 
-
-G = []
-for line in open("test.txt", 'r').read().split("\n"):
-    G.append([int(c) for c in line])
-
-R = len(G)
-C = len(G[0])
-
-def part1(G):
-    # need to use uniform cost search instead
-    start = (0, 0, 0) # risk, x, y, path
+# need to use uniform-cost search instead
+def UCS(G):
+    start = (0, 0, 0) # risk, x, y
     visited = {(0, 0)}
     Q = PriorityQueue()
     Q.put(start)
 
     while Q.qsize() > 0:
-        print(Q)
         risk, x, y = Q.get()
-        print(f'{x}, {y}, cur risk: {risk}')
+        # print(f'{x}, {y}, cur risk: {risk}')
         if (x==R-1 and y == C-1):
-            print(risk)
             return risk
 
         for xx, yy in getNeighbors(x,y):
             if (xx, yy) not in visited:
-                print(f'{x}, {y} -> {xx}, {yy}, cur risk: {risk}, new risk: {G[xx][yy] + risk}')
+                # print(f'{x}, {y} -> {xx}, {yy}, cur risk: {risk}, new risk: {G[xx][yy] + risk}')
                 visited.add((xx, yy))
                 Q.put((G[xx][yy] + risk, xx, yy))
 
@@ -45,4 +35,38 @@ def getNeighbors(x,y):
             neighbors.append((xx, yy))
     return neighbors
 
-print(part1(G))
+
+G = []
+for line in open("input.txt", 'r').read().split("\n"):
+    G.append([int(c) for c in line])
+
+R = len(G)
+C = len(G[0])
+print(f'Part 1: {UCS(G)}')
+
+G5 = []
+
+for j in range(R*5):
+    line = []
+    for i in range(5):
+        # for each tile
+        for rr in G[j%R]:
+            # for each row in original
+            new_rr = (rr + i + j//R) # j should be [0, R)
+            while (new_rr > 9): # can't use modulo here because 18%9 = 0, we want 9
+                new_rr -=9
+
+            line.append(new_rr)
+
+    # print(line)
+    G5.append(line)
+
+# g = open("input5x.txt", 'w')
+# for line in G5:
+#     g.write("".join([str(c) for c in line]))
+#     g.write("\n")
+
+# redefinition of R and C needed for part2
+R = len(G5)
+C = len(G5[0])
+print(f'Part 2: {UCS(G5)}')
